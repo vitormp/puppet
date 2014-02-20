235f0bbde1e6e4b7eda8e51c32f9b5992fdbce53
Vitor Monteiro Puente <vmonteiro@octo.com>
2014-02-20 15:04:11 -0300

    Changing the directories to be more generic

--- puppet_manifest.pp
+++ puppet_manifest.pp
@@ -1,26 +1,26 @@
 #puppet manifest
 
 file {"rah_file":
-	path	=>	'/Users/vmp/dev/puppet/temp/rah2',
+	path	=>	'/tmp/rah2',
 	ensure	=>	present,
 	content	=>	"I am a file!!!",
 }
 
 
 file {"text_file" :
-	path	=>	'/Users/vmp/dev/puppet/temp/text_file',
+	path	=>	'/tmp/text_file',
 	ensure	=>	file,
 	content	=>	"I am a file!!!"
 }
 
-file {'/Users/vmp/dev/puppet/temp/test_directory':
+file {'/tmp/test_directory':
 	ensure => directory,
 	mode   => 0644,
 }
 
-file {'/Users/vmp/dev/puppet/temp/test3':
+file {'/tmp/test3':
 	ensure => link,
-	target => '/Users/vmp/dev/puppet/temp/test1',
+	target => '/tmp/dev/puppet/temp/test1',
 }
 
 notify {"I'm notifying you.":}
